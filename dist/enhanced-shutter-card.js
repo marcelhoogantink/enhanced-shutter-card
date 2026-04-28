77cdfed6de5d701709734c45e0b0a822ab5b1284
marcelhoogantink <marcel@hoogantink.com>
2026-04-28 19:42:26 +0200

    fix: remove unnecessary counter initialization in EnhancedShutterCardNew class

--- dist/enhanced-shutter-card.js
+++ dist/enhanced-shutter-card.js
@@ -128,7 +128,7 @@ class EnhancedShutterCardNew extends LitElement{
         entitiesInGroup.forEach(entityId => {
           let newSubConfig = {...subConfig, entity: entityId, group: subConfig.entity, id: id++};
           let shutterConfig = this.#buildConfig(cardConfig,newSubConfig);
-          let counter =1;
+
           if (shutterConfig.name) {
             shutterConfig.name = shutterConfig.name.replace("@", counter++);
           }
