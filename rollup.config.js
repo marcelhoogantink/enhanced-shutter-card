f7d6674533d774729f80dfa593dd95de257f291f
Marcel Hoog Antink <51886888+marcelhoogantink@users.noreply.github.com>
2026-05-09 15:35:14 +0200

    Integrate rollup-plugin-copy for image and lit files
    Add copy plugin to rollup config for asset management

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
+import copy from "rollup-plugin-copy";
 import terser from '@rollup/plugin-terser';
 import replace from '@rollup/plugin-replace';
 
@@ -6,6 +7,19 @@ const plugins = [
   resolve(),
   replace({ preventAssignment: true, __VERSION__: process.env.GITHUB_REF_NAME ?? 'dev' }),
   terser(),
+  copy({
+    targets: [
+      {
+        src: "cards/images/**/*",
+        dest: "dist/images",
+      },
+      {
+        src: "cards/lit/**/*",
+        dest: "dist/lit",
+      },
+    ],
+    hook: "writeBundle",
+    }),
 ];
 
 export default {
