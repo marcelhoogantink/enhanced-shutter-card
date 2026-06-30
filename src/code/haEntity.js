cba971eac3a4c7cd3b68a1d6738f28dcb1e1944d
marcelhoogantink <marcel@hoogantink.com>
2026-06-30 12:00:29 +0200

    feat: Add configuration and entity handling for cover components
    - Introduced `cfg.js` to manage configuration settings for cover entities, including methods for retrieving and setting various configuration options.
    - Implemented `haEntity.js` to encapsulate Home Assistant entity data, providing methods to access state, attributes, and other relevant information.
    - Added support for sub-entities through `haSubEntity`, allowing for better management of related entities such as battery and signal.
    - Enhanced configuration handling with methods to fill and validate settings based on provided configuration objects.

---
+++ src/code/haEntity.js
@@ -0,0 +1,78 @@
+export class haEntity{
+  #state;
+  #attributes;
+  //#lastChanged;
+  //#lastUpdated;
+  // #context;
+  #entityId;
+  constructor(hass,entityId)
+  {
+    let entityInfo = hass.states[entityId];
+    if (typeof entityInfo !== "undefined") {
+      this.#state = entityInfo.state;
+      this.#attributes = entityInfo.attributes;
+      //this.#lastChanged = entityInfo.last_changed;
+      //this.#lastUpdated =  entityInfo.last_updated;
+      //this.#context =  entityInfo.context;
+      this.#entityId = entityInfo.entity_id;
+    }else{
+      console.warn('haEntity: Entity [', entityId, '] not found');
+      this.#state = C.UNAVAILABLE;
+      this.#attributes = C.UNAVAILABLE;
+      this.#entityId = entityId || C.UNAVAILABLE;
+      //this.#lastChanged = C.UNAVAILABLE;
+      //this.#lastUpdated = C.UNAVAILABLE;
+      //this.#context = C.UNAVAILABLE;
+    }
+  };
+
+  getState(){
+    return this.#state || C.UNAVAILABLE;
+  }
+  getAttributes(){
+    return this.#attributes || C.UNAVAILABLE;
+  }
+  getEntityId(){
+    return this.#entityId || C.UNAVAILABLE;
+  }
+  getCurrentPosition(){
+    return this.getAttributes()?.current_position ?? null;
+  }
+  getCurrentTiltPosition(){
+    return this.getAttributes()?.current_tilt_position ?? null;
+  }
+  getFriendlyName(){
+    return this.getAttributes()?.friendly_name ?? C.UNAVAILABLE;
+  }
+  getSupportedFeatures(){
+    return this.getAttributes()?.supported_features ?? null;
+  }
+  getUnitOfMeasurement(){
+    return this.getAttributes()?.unit_of_measurement ?? C.UNAVAILABLE;
+  }
+  isGroup(){
+    return this.getAttributes()?.entity_id !== undefined;
+  }
+}
+export class haSubEntity{
+
+  constructor(hass,type,entityId=false){
+    this.hass= hass;
+    this.type=type;
+    this.entityId = entityId;
+    //this.entity = this.set(entityId);
+    this.set(entityId);
+  }
+  set(entityId){
+    if (entityId && entityId !==C.AUTO){
+      this.entity = new haEntity(this.hass,entityId);
+      this.entityId=entityId;
+    }
+  }
+  get(){
+    return this.entity
+  }
+  update(haEntity){
+    this.entity=haEntity;
+  }
+}
